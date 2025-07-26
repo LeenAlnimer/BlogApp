ed7eaf6631e117f2ffed68d0e28765a3acfaa069
leen <programmerleen@gmail.com>
2025-07-26 01:35:06 -0700

    Advanced State

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { BlogProvider } from "./src/context/BlogContext";
+import { Provider } from "./src/context/BlogContext";
 import IndexScreen from "./src/screens/IndexScreen";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <BlogProvider>
+    <Provider>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Index">
           <Stack.Screen name="Index" component={IndexScreen} />
         </Stack.Navigator>
       </NavigationContainer>
-    </BlogProvider>
+    </Provider>
   );
 }
--- src/context/createDataContext.js
+++ src/context/createDataContext.js
@@ -3,8 +3,15 @@ export default (reducer, actions, initialState) => {
   const Context = React.createContext();
   const Provider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, initialState);
+    const boundActions = {};
+    for (let key in actions) {
+      boundActions[key] = actions[key](dispatch);
+    }
+
     return (
-      <Context.Provider value={{ state: state }}>{children}</Context.Provider>
+      <Context.Provider value={{ state, ...boundActions }}>
+        {children}
+      </Context.Provider>
     );
   };
   return { Context, Provider };
